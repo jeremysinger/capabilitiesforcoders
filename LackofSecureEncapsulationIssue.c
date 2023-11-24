cddf02a0f704a0c6f258677d37de5296536b5752
Jeremy Singer <giraffe21@gmail.com>
2023-11-24 11:01:56 +0000

    Update LackofSecureEncapsulationIssue.c
    fix up some syntax errors

--- LackofSecureEncapsulationIssue.c
+++ LackofSecureEncapsulationIssue.c
@@ -2,44 +2,30 @@
 #include<stdlib.h>
 
 struct Student {
-
-char name[100];
-
-int age;
-
+    char name[100];
+    int age;
 };
 
-struct Student* create_student(char name*, int age);
-
-struct Student* student = (struct Student*)malloc(sizeof(struct Student));
-
-if(student){
-
-strcpy(student->name, name);
-
-student->age = age; 
-
-}
-
-return student; 
-
+struct Student* create_student(char name*, int age) {
+    struct Student* student = (struct Student*)malloc(sizeof(struct Student));
+    if(student){
+        strcpy(student->name, name);
+        student->age = age; 
+    }
+    return student; 
 }
 
 int main() {
+    struct Student* student1 = create_student("Alice", 20);
+    struct Student* student2 = create_student("Bob", 22);
 
-struct Student* student1 = create_student("Alice", 20);
+    // unintentional access to students data (no encapsulation)
+    printf("student1: %s,%d\n", student2->name, student2->age);
 
-struct Student* student2 = create_student("Bob", 22);
+    free(student1);
+    free(student2);
 
-//unintentional access to students data (no encapsulation)
-
-printf("student1: %s,%d\n", student2->name, student2->age);
-
-free(student1);
-free(student2);
-
-return 0;
-
+    return 0;
 }
 
 
